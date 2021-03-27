a389a1a4ebae4ed863e8f8bc294cd3357855af19
Sergio D Perdomo <sergio.931129@gmail.com>
2021-03-27 00:08:08 -0500

    Add sign in status bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def show_sign_in_out
+		out = ''
+		if logged_in?
+			out << link_to('My Profile', user_path(session[:user_id]))
+			out << link_to('Sign out', logout_path, method: :delete)
+		else
+			out << link_to('login', sign_in_path)
+			out << link_to('Sign up', new_user_path)
+		end
+
+		out.html_safe
+	end
 end
